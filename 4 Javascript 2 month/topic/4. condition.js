5cd1b57c7d3d4c8fee4e7ed4a4870e28c22f9a4f
Ravi Singh <111888352+ravisingh007ravi@users.noreply.github.com>
2026-02-12 15:47:49 +0530

    a

---
+++ 4 Javascript 2 month/topic/4. condition.js
@@ -0,0 +1,22 @@
+// condition 
+
+// condition base on boolean
+// true  => 1
+// false => 0
+// empty string == 0
+// empty string == false
+// 0 == false
+// 1 == true
+
+// console.log(0, false, 0 == false)
+// == compare value only
+// === compare value and data type
+
+console.log("a" == "A")
+console.log("a" === "A")
+console.log("1" == true)
+console.log("1" === "1")
+console.log("a" == 0)
+console.log("a" === 0)
+console.log(0 == false)
+console.log(0 === false)
