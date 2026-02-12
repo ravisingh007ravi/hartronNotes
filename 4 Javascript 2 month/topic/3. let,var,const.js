41bb5e6d77f9f5045502ee4e1ad4fb42ae9ee6ef
Ravi Singh <111888352+ravisingh007ravi@users.noreply.github.com>
2026-02-12 15:34:31 +0530

    aa

---
+++ 4 Javascript 2 month/topic/3. let,var,const.js
@@ -0,0 +1,56 @@
+// let , var and const 
+// let and const are block and local level scope 
+// var is global level scope
+
+// a = 10
+
+// a = a + 1
+// a = 2
+// console.log(a)
+// let A = 3
+// {
+//     let a = 1
+//     console.log(a)
+//     let A = 2
+//     A = 20
+//     console.log(A,a)
+// }
+
+
+// {
+//     let a = 2 
+//     console.log(a)
+// }
+
+// {   
+//     console.log(A)
+//     let A = 99
+// }
+
+
+
+// let a = 20
+// console.log(a)
+
+// const a = 3
+// a = 90
+// console.log(a)
+// var a = 89
+// {
+//     console.log(a)
+//     var a = 9
+// }
+
+// console.log(a)
+// var a =3
+// var a =3
+// var a =3
+// var a =38
+
+let a = 10
+
+{
+    console.log(a)
+}
+
+
