526b7f30803e44acd66ff5a7d6a0622ab1b16faa
Ravi Singh <111888352+ravisingh007ravi@users.noreply.github.com>
2026-02-13 15:58:42 +0530

    ss

---
+++ 4 Javascript 2 month/Assigment Solution/1 Day if else.js
@@ -0,0 +1,61 @@
+// Assignment - Day 1
+
+// 1. Write a program that determines whether a given number is positive or negative.
+
+
+
+{
+    let n = 'r'
+
+    if (n > 0) {
+        console.log("positive")
+    }
+    else if (n == 0) {
+        console.log("zero")
+    }
+    else {
+        console.log("negative")
+    }
+
+}
+
+
+
+
+// 2. Write a program that checks if a number is even or odd.
+
+
+
+// 3. Write a program to determine the greater of two numbers.
+
+// 4. Suppose we need to assign different grades to students based on their scores:
+//    • If a student scores between 91–100, assign grade A.
+//    • If a student scores between 81–90, assign grade B.
+//    • If a student scores between 71–80, assign grade C.
+//    • If a student scores between 61–70, assign grade D.
+//    • Otherwise, assign Fail.
+
+// 5. Write a program that calculates the ticket price based on age with the following conditions:
+//    • Age below 12: ticket price is 5.
+//    • Age below 18: ticket price is 10.
+//    • Age below 60: ticket price is 20.
+//    • Age above 60: ticket price is 15.
+
+// 6. Write a program that determines if a year is a leap year.
+
+// 7. Write a program that calculates a discount based on the purchase amount:
+//    • Price equal to or over 100: discount is 20.
+//    • Price equal to or over 50: discount is 10.
+//    • Otherwise, discount is 0.
+
+// 8. Write a program that greets the user based on the time of day.
+//    Display "Good Morning", "Good Afternoon", or "Good Evening" based on the time when you run the code.
+
+// 9. Write a program that calculates the Body Mass Index (BMI) and categorizes it.
+//    The formula for BMI is: weight / (height * height).
+
+// 10. Write a simple number guessing game.
+//     Provide a secret number and a guess. Based on those numbers, give the player clues if their guess is higher, lower, or correct.
+
+// 11. Write a program that checks if a given string is a palindrome.
+//     A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., "racecar", "madam", "nurses run".                     
---
+++ 4 Javascript 2 month/topic/5. if else.js
@@ -0,0 +1,26 @@
+// let n  = 50
+
+// if(n>2){
+//     console.log("A")
+// }
+
+// if(n==2){
+//     console.log('c')
+// }
+// else{
+//     console.log("B")
+// }
+
+// if (10 > 10) {
+//     console.log('a')
+// }
+// else if (2 >= 20) {
+//     console.log('b')
+// }
+// else if (10 < 5) {
+//     console.log('c')
+// }
+// else {
+//     console.log("Hiii")
+// }
+
