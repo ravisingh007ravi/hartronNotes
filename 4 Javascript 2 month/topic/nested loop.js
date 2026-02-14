6fae130107efcd7f39e82699996229fa75b03605
Ravi Singh <111888352+ravisingh007ravi@users.noreply.github.com>
2026-02-14 16:38:09 +0530

    dsfsd

--- 4 Javascript 2 month/topic/nested loop.js
+++ 4 Javascript 2 month/topic/nested loop.js
@@ -1,11 +1,63 @@
-let n = 3
-let bag = ''
-for (let i = 0; i < n; i++) {
-    for (let j = i; j < n; j++) {
-        bag = bag + '* '
-    }
-    bag=bag+'\n'
+
+
+// let a = 1
+// a = a + 1
+// a = a + 1
+// ++a
+// a++
+// console.log(a)
+// console.log(a++)
+// console.log(++a)
+
+
+// let n = 1
+// let a = ''
+// let b = 0
+// n++
+// console.log(n,typeof(n))
+// ++n
+// console.log(n,typeof(n))
+// n+=1
+// console.log(n,typeof(n))
+// n+=a
+// console.log(n,typeof(n))
+// n+=b
+// console.log(n,typeof(n))
+// n+=1
+// console.log(n,typeof(n))
+
+// console.log(1*'1a'*1*1)
+
+
+// console.log(-Infinity>0)
+
+
+// let year = 2004
+// if (year % 4 == 0 && year % 100 != 0) {
+//     console.log('l')
+// }
+// else if (year % 400 == 0) {
+//     console.log('l')
+// }
+// else {
+//     console.log('n')
+// }
+
+
+
+let a = 10
+let b = 2
+let c = 3
+let d = 3
+if(a>b && a>c && a>d){
+    console.log('a')
 }
-console.log(bag)
-
-
+else if(b>a && b>c && b>d){
+    console.log('B')
+}
+else if(c>a && c>b && c>d){
+    console.log('C')
+}
+else{
+    console.log('D')
+}
