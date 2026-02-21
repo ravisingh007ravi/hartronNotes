41cb086b3327ee852034574f06adcbadcfe70e2d
Ravi Singh <111888352+ravisingh007ravi@users.noreply.github.com>
2026-02-21 15:38:49 +0530

    dsad

--- 4 Javascript 2 month/topic/6. nested loop.js
+++ 4 Javascript 2 month/topic/6. nested loop.js
@@ -18,32 +18,65 @@
 
 // Credit card Apply Process 
 
-let salaryType = true
-let income = 30000
-let adharCard = false
-let panCard = false 
+// let salaryType = true
+// let income = 30000
+// let adharCard = false
+// let panCard = false 
 
 
-if(salaryType){
-    if(adharCard){
-        if(panCard){
-            if(income>=20000){
-                console.log('Get Credit card')
-            }
-            else{
-                console.log('Fail')
-            }
-        }
-        else{
-            console.log('Pan Card is Required..')
-        }
-    }
-    else{
-        console.log('AdharCard is Require...')
-    }
-}
-else{
-    console.log('Not Apply')
-}
+// if(salaryType){
+//     if(adharCard){
+//         if(panCard){
+//             if(income>=20000){
+//                 console.log('Get Credit card')
+//             }
+//             else{
+//                 console.log('Fail')
+//             }
+//         }
+//         else{
+//             console.log('Pan Card is Required..')
+//         }
+//     }
+//     else{
+//         console.log('AdharCard is Require...')
+//     }
+// }
+// else{
+//     console.log('Not Apply')
+// }
 
 
+let course = 'AI'
+let exStd = false
+let interns = true
+let pass = false
+let placmentG = true
+let paymentType = 'cash'
+let emiTime = 3
+
+course == 'AI' || course == "CS" || course == 'DM' || course == 'DA' ?
+    exStd ?
+        paymentType == 'cash' ? console.log('20% course Discount and 10% cash Discount', course) :
+            emiTime == 3 ? console.log('English course Free') :
+                emiTime == 6 ? console.log("2% discount", course) :
+                    console.log('No Discount', course)
+        :
+        interns ? pass ?
+            paymentType == 'cash' ? console.log('10% course Discount and 10% cash Discount', course) :
+                emiTime == 3 ? console.log('English course Free') :
+                    emiTime == 6 ? console.log("2% discount", course) :
+                        console.log('No Discount', course)
+            :
+            paymentType == 'cash' ? console.log('10%', course) :
+                emiTime == 3 ? console.log('English course Free') :
+                    emiTime == 6 ? console.log("2% discount", course) :
+                        console.log('No Discount', course)
+            :
+            paymentType == 'cash' ? console.log('10%', course) :
+                emiTime == 3 ? console.log('English course Free') :
+                    emiTime == 6 ? console.log("2% discount", course) :
+                        console.log('No Discount', course)
+    : console.log("not found")
+
+    
