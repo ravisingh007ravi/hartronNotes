6c7cf3fd6172bf3980486ebe7da199c269ab11c1
Ravi Singh <ravisingh007ravi@gmail.com>
2026-05-12 06:31:06 +0000

    sdf

--- Batchs/batch-11to1/JavaScript/topic/2. Operater.js
+++ Batchs/batch-11to1/JavaScript/topic/2. Operater.js
@@ -1,6 +1,6 @@
 // type of Operater
 // 1. Arithmetic Operators (+ - * / %)
-// 2. Assignment Operators (=, +=, -=, *=, /=, %=, ++, --)
+// 2. Assignment Operators (=, +=, -=, *=, /=, %=, **=, ++, --)
 // 3. Comparison Operators (==, ===, !=, !==, >, <, >=, <=)
 // 4. Logical Operators  (&&, ||, !)
 // 5. Bitwise Operators (&, |, ^, ~, <<, >>, >>>)
@@ -26,3 +26,24 @@
 // console.log('mod of a and b = ', mod); 
 
 // 2. Assignment Operators (=, +=, -=, *=, /=, %=, ++, --)
+
+// a = 2
+// b = 2
+
+// a = a + b // first way value assign
+// console.log(a);
+// a **= b // first way value assign
+// console.log(a);
+
+// a = a + 1
+// a =- 10 + a //459 a = a *(10+17)
+// a++
+// console.log(a);
+
+
+// 3. Comparison Operators (==, ===, !=, !==, >, <, >=, <=)
+
+a = 20
+b = 10
+console.log(a !== b); // compare only value not data type
+console.log(a === b); // compare both value and data type
