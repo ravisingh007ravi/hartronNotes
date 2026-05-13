d83b456ed796d00b8e9e9b1793c92a63337cfd17
Ravi Singh <ravisingh007ravi@gmail.com>
2026-05-13 05:18:52 +0000

    sign connect to BD

--- Batchs/batch12to2/backend/src/index.js
+++ Batchs/batch12to2/backend/src/index.js
@@ -3,4 +3,17 @@ import express from 'express'
 const app = express()
 const port = 8080
 
-app.listen(port,()=>console.log(`server is running = ${port}`)) 
+const abc =(req,res)=>{
+    res.send("ok")
+}
+
+const bb =(req,res)=>{
+    res.send({name:'sad',age:"34"})
+}
+
+app.get('/a',abc)
+app.get('/b',bb)
+
+app.listen(port, () => console.log(`server is running = ${port}`))
+
+
