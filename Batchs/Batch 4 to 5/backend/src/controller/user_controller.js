406de393188b051479d612aa6014411653e1564a
ravi singh <ravisingh007ravi@gmail.com>
2026-06-08 10:45:53 +0530

    Fdsaf

--- Batchs/Batch 4 to 5/backend/src/controller/user_controller.js
+++ Batchs/Batch 4 to 5/backend/src/controller/user_controller.js
@@ -1,15 +1,31 @@
 import { user_model } from '../model/user_model.js'
 import { errorhandling } from '../error/allerror.js'
+import CryptoJS from 'crypto-js'
 export const create_user = async (req, res) => {
   try {
     const data = req.body
+    const { fname, lname, email, gender, mobile, password } = data
 
-    data.role = 'user'
-    const db = await user_model.create(data)
-    res.status(200).send({ status: true, msg: "ok....", db })
+    const otp = Math.floor(Math.abs(CryptoJS.lib.WordArray.random(2).words[0]) % 9000) + 1000;
+    const otpExpiryTime = Date.now() + 1000 * 60 * 5
+    const checkUser = await user_model.findOne({ email: email })
+    const normalizedEmail = email.replace(/\./g, '').replace(/\+.*(?=@)/, '');
+    if (checkUser) {
+      if ((checkUser.verification.user.isVerify)) return res.status(200).send({ status: true, msg: "Account Already Create Pls LogIn" })
+      await user_model.findOneAndUpdate({ email: normalizedEmail }, { $set: { 'verification.user': { otp, otpExpiryTime } } })
+      return res.status(200).send({ status: true, msg: "Resend Otp Successfully" })
+    }
+
+    const uploadData = {
+      fname, lname, email: normalizedEmail, gender, mobile, role: 'user', password,
+      verification: { user: { otp, otpExpiryTime } }
+    }
+
+    const db = await user_model.create(uploadData)
+    res.status(200).send({ status: true, msg: "Account Create Successfully", data: { fname, lname, email, gender, mobile }, id: db._id })
   }
 
-  catch (err) { errorhandling(err,res) }
+  catch (err) { errorhandling(err, res) }
 }
 
 export const verify_otp = async (req, res) => {
