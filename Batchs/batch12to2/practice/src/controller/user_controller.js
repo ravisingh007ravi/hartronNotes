3b1529b652898a227b56bc106b5e7d74bd06425a
Ravi Singh <ravisingh007ravi@gmail.com>
2026-07-09 08:43:14 +0530

    aa

--- Batchs/batch12to2/practice/src/controller/user_controller.js
+++ Batchs/batch12to2/practice/src/controller/user_controller.js
@@ -77,13 +77,13 @@ export const user_otp_verify = async (req, res) => {
         const otp = req.body?.otp
 
         const OTP_LOCK_LADDER = [
-            1000 * 60, 
-            1000 * 60 * 5, 
+            1000 * 60,
+            1000 * 60 * 5,
             1000 * 60 * 10,
-            1000 * 60 * 60, 
+            1000 * 60 * 60,
             1000 * 60 * 60 * 24,
-            1000 * 60 * 60 * 24 * 7, 
-            1000 * 60 * 60 * 24 * 365, 
+            1000 * 60 * 60 * 24 * 7,
+            1000 * 60 * 60 * 24 * 365,
             1000 * 60 * 60 * 24 * 365 * 10,
 
         ]
@@ -157,7 +157,7 @@ export const user_otp_verify = async (req, res) => {
 
             await user_model.findByIdAndUpdate(id, {
                 $set: {
-                    'verification.user.otpAtm': 1, 
+                    'verification.user.otpAtm': 1,
                     'verification.user.otpLockUntil': lockUntil,
                     'verification.user.otpLockStage': nextStage,
                 }
@@ -180,4 +180,70 @@ export const user_otp_verify = async (req, res) => {
     catch (err) {
         error_handling(err, res)
     }
+}
+
+export const resend_otp = async (req, res) => {
+    try {
+
+        const { id } = req.params
+
+        if (!id) return res.status(400).send({ status: false, message: 'Id is Required' })
+
+        const checkEmail = await user_model.findById(id)
+            .select({ email: 1, fname: 1, verification: 1 })
+        let otp = crypto.randomInt(1000, 9999)
+        const otpExpiryTime = Date.now() + 1000 * 60 * 5
+
+
+        if (!checkEmail) return res.status(404).send({ status: false, message: 'User not found' })
+
+
+        if (checkEmail) {
+            if (checkEmail?.verification?.user?.isVerify) {
+                return res.status(400).send({ status: false, message: 'Account Already Exists... Pls Login' })
+            }
+
+            const lockUntil = checkEmail?.verification?.user?.otpLockUntil
+            if (lockUntil && Date.now() < lockUntil) {
+
+                const d = new Date(lockUntil);
+
+                const formatted =
+                    `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')} ` +
+                    `${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}:${String(d.getSeconds()).padStart(2, '0')}`;
+
+                return res.status(429).send({
+                    status: false,
+                    message: 'Too many wrong attempts. Pls try again after some time',
+                    data: { id: checkEmail._id }, lockTime: formatted, RawDate: lockUntil
+
+                })
+            }
+
+            await user_model.findOneAndUpdate(
+                { _id: id },
+                {
+                    $set: {
+                        'verification.user.otp': otp,
+                        'verification.user.otpExpiryTime': otpExpiryTime,
+                    }
+                }
+            )
+            user_otp_verification_mail(checkEmail.fname, checkEmail.email, otp)
+            return res.status(400).send({
+                status: false, message: 'Pls Verify Your Email',
+                data: { id: checkEmail._id, email: checkEmail.email }
+            })
+        }
+    }
+
+    catch (err) { error_handling(err, res) }
+}
+
+export const user_login = async (req, res) => {
+    try {
+
+    }
+
+    catch (err) { error_handling(err, res) }
 }
--- Batchs/batch12to2/practice/src/routes/routes.js
+++ Batchs/batch12to2/practice/src/routes/routes.js
@@ -1,6 +1,6 @@
 import exress from 'express'
 import multer from 'multer'
-import { create_user, user_otp_verify } from '../controller/user_controller.js'
+import { create_user, user_otp_verify,resend_otp } from '../controller/user_controller.js'
 
 const upload = multer({ storage: multer.diskStorage({}) });
 
@@ -9,3 +9,4 @@ export const router = exress.Router()
 // user API's
 router.post('/user/create_user', upload.single('userImg'), create_user)
 router.post('/user/user_otp_verify', user_otp_verify)
+router.get('/user/resend_otp/:id', resend_otp)
